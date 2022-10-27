8cc3832063cf146b4859404abbefe045104d294d
Guilherme <guilherme.zanus@gmail.com>
2022-10-27 17:05:04 -0300

    Criação da classe Produto, ProdutoController e ProdutoRepository

---
+++ exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/ProdutoController.java
@@ -0,0 +1,26 @@
+package br.com.cod3r.exerciciossb.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.cod3r.exerciciossb.models.entities.Produto;
+import br.com.cod3r.exerciciossb.models.repositories.ProdutoRepository;
+
+@RestController
+@RequestMapping("/api/produtos")
+public class ProdutoController {
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
+	@PostMapping
+	public @ResponseBody Produto novoProduto(Produto produto ) {
+		
+		produtoRepository.save(produto);
+		
+		return produto;
+	}
+}
