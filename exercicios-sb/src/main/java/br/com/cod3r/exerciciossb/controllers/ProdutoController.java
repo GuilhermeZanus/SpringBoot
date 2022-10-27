bacfcb2c89dc7fddcdc2aab44d2c7957f3abc141
Guilherme <guilherme.zanus@gmail.com>
2022-10-27 17:58:56 -0300

    Atualizações nas classes Produto e ProdutoController

--- exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/ProdutoController.java
+++ exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/ProdutoController.java
@@ -1,7 +1,14 @@
 package br.com.cod3r.exerciciossb.controllers;
 
+import java.util.Optional;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,10 +24,29 @@ public class ProdutoController {
 	private ProdutoRepository produtoRepository;
 	
 	@PostMapping
-	public @ResponseBody Produto novoProduto(Produto produto ) {
+	public @ResponseBody Produto novoProduto(@Valid Produto produto ) {
 		
 		produtoRepository.save(produto);
 		
 		return produto;
 	}
+	
+	@GetMapping
+	public Iterable<Produto> obterProdutos() {
+		return produtoRepository.findAll();
+	}
+	
+	@GetMapping(path="/{id}")
+	public Optional<Produto> obterProdutoPorId(@PathVariable int id) {
+		return produtoRepository.findById(id);
+	}
+	
+	@PutMapping
+	public Produto alterarProduto(@Valid Produto produto) {
+		produtoRepository.save(produto);
+		return produto;
+	}
+	
+	
+	
 }
