173f9c5c23bd7962207ebcd2a4aba0a4a7d0ef57
Guilherme <guilherme.zanus@gmail.com>
2022-10-31 21:31:34 -0300

    Atualizando a classe ProdutoController com a busca de produtos por nome

--- exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/models/repositories/ProdutoRepository.java
+++ exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/models/repositories/ProdutoRepository.java
@@ -6,4 +6,5 @@ import br.com.cod3r.exerciciossb.models.entities.Produto;
 
 public interface ProdutoRepository extends PagingAndSortingRepository<Produto, Integer> {
 	
+	public Iterable<Produto> findByNomeContainingIgnoreCase(String parteNome);
 }
