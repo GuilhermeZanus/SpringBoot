4b00e8d3e282cae3d83d8cbd1609b1b903f16493
Guilherme <guilherme.zanus@gmail.com>
2022-10-31 21:15:30 -0300

    Atualização da classe ProdutoController

--- exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/ProdutoController.java
+++ exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/ProdutoController.java
@@ -5,11 +5,13 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +25,8 @@ public class ProdutoController {
 	@Autowired
 	private ProdutoRepository produtoRepository;
 	
-	@PostMapping
+//	@PostMapping
+	@RequestMapping(method = {RequestMethod.POST, RequestMethod.PUT})
 	public @ResponseBody Produto novoProduto(@Valid Produto produto ) {
 		
 		produtoRepository.save(produto);
@@ -36,17 +39,28 @@ public class ProdutoController {
 		return produtoRepository.findAll();
 	}
 	
+	@GetMapping(path = "/pagina/{numeroPagina}/{qtdePagina}")
+	public Iterable<Produto> obterProdutosPorPagina(
+			@PathVariable int numeroPagina, @PathVariable int qtdePagina) {
+		if(qtdePagina >=5) qtdePagina = 5;
+		Pageable page = PageRequest.of(numeroPagina, qtdePagina);
+		return produtoRepository.findAll(page);
+	}
+	
 	@GetMapping(path="/{id}")
 	public Optional<Produto> obterProdutoPorId(@PathVariable int id) {
 		return produtoRepository.findById(id);
 	}
 	
-	@PutMapping
-	public Produto alterarProduto(@Valid Produto produto) {
-		produtoRepository.save(produto);
-		return produto;
+//	@PutMapping
+//	public Produto alterarProduto(@Valid Produto produto) {
+//		produtoRepository.save(produto);
+//		return produto;
+//	}
+	
+	@DeleteMapping(path="/{id}")
+	public void excluirProduto(@PathVariable int id) {
+		produtoRepository.deleteById(id);
 	}
-	
-	
 	
 }
